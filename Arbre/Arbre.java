a37f4151f98001056d74746137cb2dd9424d319e
FitiaLalaina <fitialalainarad@gmail.com>
2023-04-15 23:05:26 +0300

    Update Arbre.java

--- Arbre/Arbre.java
+++ Arbre/Arbre.java
@@ -1,5 +1,17 @@
 package arbre;
 
 public class Arbre {
+    public double[] trieAcs(double[] table){
+        for (int j = 0; j < table.length; j++) {
+            for (int i = j + 1; i < table.length; i++) {
+                if(table[i] < table[j]){
+                    double temp = table[i];
+                    table[i] = table[j];
+                    table[j] = temp;
+                }
+            }
+        }
+        return table;
+    }
     
 }
